b50c7896d2ff7d85221615a4fe728e58e8fcdf9b
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 23:35:31 +0530

    Added : Math FN ,Random FN

--- src/main/java/org/example/Math.java
+++ src/main/java/org/example/Math.java
@@ -3,6 +3,8 @@ public class Math {
     public static void main(String[] args) {
         System.out.println(Math.max(10,11));//prints max
         System.out.println(Math.min(10,11));//prints min
+        System.out.println(Math.random());//random number generator
+        System.out.println((int)Math.random());//type cast integer
 
     }
 
