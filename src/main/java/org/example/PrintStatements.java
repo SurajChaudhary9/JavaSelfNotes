5a93c04c8b3d70b4fabf227f683de910585a1a45
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 15:31:06 +0530

    Print Statements

---
+++ src/main/java/org/example/PrintStatements.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class PrintStatements {
+    public static void main(String[] args) {
+        //1
+        System.out.println("OUTPUT 1:");
+        System.out.println("I Love Java Programming");
+        //2
+        System.out.println("OUTPUT 2 :");
+        System.out.println("My name is Peter");
+        System.out.println("I am learning Java");
+        System.out.println("I like Coffee");
+        //3
+
+        String str = "I Like Coffee";// String variable
+        int count = 2; //int
+        double cost = 99.00;//double
+        System.out.println("OUTPUT 3 :");
+        //Print all data on console
+        System.out.println(str);
+        System.out.println(count);
+        System.out.println(cost);
+        //4
+        String str1 = " Coffee!!!";
+        int count1 = 100;
+        System.out.println("OUTPUT 4 :");
+        System.out.println(count1+str1);
+        //5 multi sop imp
+        int num = 100;
+
+        System.out.println(" o/p 5: value = "+ num);
+
+    }
+}
--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -5,6 +5,7 @@ public class Start {
     public static void main(String[] args) {
         //Non primitive
         String name = "Aman";
+
         System.out.println(name.length());
         //size in infinite upto memory // length is funcn returns length
         System.out.println("O/P 1 :"+name.length());
