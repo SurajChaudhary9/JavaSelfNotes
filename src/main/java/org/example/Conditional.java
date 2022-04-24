fe09c94b2bde5cf6997c6bcb109bfc9c233455b5
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-25 00:13:04 +0530

    Added : conditional statement - Boolean

---
+++ src/main/java/org/example/Conditional.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Conditional {
+    public static void main(String[] args) {
+        //conditional statement    boolean sunUp = true;
+        boolean sunUp = true;
+        if (sunUp == true)
+            System.out.println("Its a Day");
+        else
+            System.out.println("Its night");
+    }
+}
