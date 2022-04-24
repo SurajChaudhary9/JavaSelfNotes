d38a4ee6ad7a866748a37a9c2ae51cf261d39387
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 15:18:43 +0530

    combining two SOP'S

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -7,6 +7,7 @@ public class Start {
         String name = "Aman";
         System.out.println(name.length());
         //size in infinite upto memory // length is funcn returns length
-
+        System.out.println("O/P 1 :"+name.length());
+        //"O/P 1 :"+name.length() - COBINING TWO SOP'S USING "" +
     }
 }
