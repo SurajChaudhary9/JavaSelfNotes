b92f74562f66d08d8e6d7434b25c270d3f64ef00
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-25 01:45:50 +0530

    Refactored : Boolean

--- src/main/java/org/example/Conditional.java
+++ src/main/java/org/example/Conditional.java
@@ -3,22 +3,22 @@ package org.example;
 public class Conditional {
     public static void main(String[] args) {
         //conditional statement    boolean sunUp = true;
-//        boolean sunUp = true;
-//        if (sunUp == true)
-//            System.out.println("Its a Day");
-//        else
-//            System.out.println("Its night");
+        boolean isAdult = true;
+        if (isAdult)//by default assumes true no need to define
+            System.out.println("Its a Adult");
+        else
+            System.out.println("Not Adult");
 
         //Logical Operators
-        int a = 50;
-        int b = 110;
-//        if(a < 100 && b < 100) //AND operator both condition should be true to enter if loop
-//            System.out.println("Both are Less than 100");
+//        int a = 50;
+//        int b = 110;
+////        if(a < 100 && b < 100) //AND operator both condition should be true to enter if loop
+////            System.out.println("Both are Less than 100");
+////        else
+////            System.out.println("Either one is more than 100");
+//        if(a < 100 || b < 100) //OR operator Atleast one should be true
+//            System.out.println("Either one is True");
 //        else
-//            System.out.println("Either one is more than 100");
-        if(a < 100 || b < 100) //OR operator Atleast one should be true
-            System.out.println("Either one is True");
-        else
-            System.out.println("None is true");
+//            System.out.println("None is true");
     }
 }
