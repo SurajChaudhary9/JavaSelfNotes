78e3fd01d16561b379f5bd415b539ef8790f8f08
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 21:47:54 +0530

    Added : Constants uses of 'Final' Keyword

--- src/main/java/org/example/Casting.java
+++ src/main/java/org/example/Casting.java
@@ -10,5 +10,11 @@ public class Casting {
         int fp = p + (int)18.0; //does not allow double in int
         //use(int) for acceptance
         System.out.println(fp);
+
+        //constants
+//        int age =30;
+//        age= 31;    //here values can change as it is not a constant
+        final float PI = 3.14F;
+        //PI = 1.1F; //NOW THIS WILL GIVE WARNING AS (final) keyword is used
     }
 }
