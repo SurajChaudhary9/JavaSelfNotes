3f36b2b57284bf68fa1d3037ae76e2ab8e55d5b6
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 15:15:09 +0530

    Non primitive data types

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,22 +1,12 @@
 package org.example;
 
 public class Start {
-    //Primitive Data-Types
+    //Non Primitive Data-Types
     public static void main(String[] args) {
-        //byte - 1 [-128 to 128]
-        //short - 2
-        //int - 4
-        //long -8
-        //float -4
-        //doble - 8
-        //char - 8
-        //boolean - 2 a,b,c,d;
-        byte age = 30;
-        int phone = 7709911506; //too long
-        long phone2 =7709911506L; //LONG l FOR define
-        float pi =3.14F;
-        chat letter ='@';
-        boolean isAdult = false;
+        //Non primitive
+        String name = "Aman";
+        System.out.println(name.length());
+        //size in infinite upto memory // length is funcn returns length
 
     }
 }
