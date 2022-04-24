c47f7e27b372d25b64d344229ac51c64bf2c5d81
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-25 00:17:51 +0530

    Added : conditional statement - Logical Operators

--- src/main/java/org/example/Conditional.java
+++ src/main/java/org/example/Conditional.java
@@ -3,10 +3,18 @@ package org.example;
 public class Conditional {
     public static void main(String[] args) {
         //conditional statement    boolean sunUp = true;
-        boolean sunUp = true;
-        if (sunUp == true)
-            System.out.println("Its a Day");
+//        boolean sunUp = true;
+//        if (sunUp == true)
+//            System.out.println("Its a Day");
+//        else
+//            System.out.println("Its night");
+
+        //Logical Operators
+        int a = 50;
+        int b = 110;
+        if(a < 100 && b < 100) //AND operator
+            System.out.println("Both are Less than 100");
         else
-            System.out.println("Its night");
+            System.out.println("Either one is more than 100");
     }
 }
