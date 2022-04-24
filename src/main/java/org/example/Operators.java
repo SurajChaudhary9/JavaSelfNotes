cb86886c17f18dcde7fc72eee489fbdb9756286d
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 22:36:53 +0530

    Added : Operators + - / % *

---
+++ src/main/java/org/example/Operators.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b =20;
+        int add=a+b;
+        int sub=b-a;
+        int mul=a*b;
+        int div=b/a;
+        double x = 20;
+        double y = 10;
+        double div1=y/x;
+        double modulo = a % b; //mod - remainder
+        System.out.println("Addition is :"+add);
+        System.out.println("Subtraction is : "+sub);
+        System.out.println("Multiplication is :"+mul);
+        System.out.println("Division is :"+div);
+        System.out.println("Division is for double:"+div1);//returns in decimal
+        System.out.println("Modulo is :"+modulo);
+
+    }
+}
