09a48a0a5c3a17e0ee3af8d329df700b74691fb1
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 18:47:37 +0530

    Added : Array sort length

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Array {
     public static void main(String[] args) {
         int[] marks = new int[3]; //new because non-primitive
@@ -7,5 +9,11 @@ public class Array {
         marks[1] = 99;
         marks[2] = 99;
         System.out.println(marks[2]);
+        //if commented marks 6,7,8 line o/p will be 0
+        System.out.println(marks.length); //length 3
+        System.out.println(marks[0]);//prints 95 before sort
+        Arrays.sort(marks);
+        System.out.println(marks[0]);//prints 95 in ascending order after sort
+
     }
 }
