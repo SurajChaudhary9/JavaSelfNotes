5c13887dc09220744c10083080983933c7da43c0
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 18:29:48 +0530

    Added : Funtion of sop(print)

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,6 +1,4 @@
 package org.example;
-
-
 public class Start {
     //Non Primitive Data-Types
     public static void main(String[] args) {
@@ -15,5 +13,14 @@ public class Start {
         //new keywords are using to create object of classes
         //reference
         System.out.println(name.charAt(1)); //charAt print m ...index starts from 0
+        String name2 = name.replace('m','l');//replace
+        System.out.println(name2);
+        System.out.println(name);//string are immutable(does not change)
+
+        //Index print from a string
+        String name3 = "Karan and Arjun";
+        System.out.println(name3.substring(0,5));//karan
+        System.out.println(name3.substring(6,10));//and
+
     }
 }
