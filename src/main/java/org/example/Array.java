6c7ad244b49b1d9a94bf5d8182e350c550030b3a
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 18:41:08 +0530

    Added : ARRAY

---
+++ src/main/java/org/example/Array.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3]; //new because non-primitive
+        marks[0] = 100;
+        marks[1] = 99;
+        marks[2] = 99;
+        System.out.println(marks[2]);
+    }
+}
