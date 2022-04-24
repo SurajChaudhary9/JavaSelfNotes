db2c7d6f4e68781d2a179c1c896d780ac0fc6351
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-24 23:29:34 +0530

    Added : Math Function

---
+++ src/main/java/org/example/Math.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Math {
+    public static void main(String[] args) {
+        System.out.println(Math.max(10,11));//prints max
+        System.out.println(Math.min(10,11));//prints min
+
+    }
+
+}
