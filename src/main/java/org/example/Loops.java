93c37f44a7592279576632f6dcc5e8db8cc27856
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-01 15:45:05 +0530

    Added : LOOPS WHILE

--- src/main/java/org/example/Loops.java
+++ src/main/java/org/example/Loops.java
@@ -3,9 +3,13 @@ package org.example;
 public class Loops {
     public static void main(String[] args) {
 //        int i = 0;
-        for (int i = 100; i >=1; i=i-1) {
-            System.out.println(i);
-        }
+//        for (int i = 100; i >=1; i=i-1) {
+//            System.out.println(i);
+//        }
+        int i =10;
+        while(i>1)
+        System.out.println(i);
+        i--;
     }
 }
 //prints 100 - 1
