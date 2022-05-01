324e82a3db16c52e66e6a9888b52d3317d0cb4ad
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-01 15:18:29 +0530

    Added : Case (Switch statement) with second type 3 with break

--- src/main/java/org/example/Program2.java
+++ src/main/java/org/example/Program2.java
@@ -6,17 +6,20 @@ public class Program2 {
         switch(day){
             case 1:
                 System.out.println("Monday");
+                break;
             case 2:
                 System.out.println("Tuesday");
+                break;
             case 3:
                 System.out.println("Wednesday");
+                break;
             default:
                 System.out.println("Thursday to sunday");
         }
     }
 }
 //Notes :
-/*
+/*Break statment gives individual block output and exists loops
 o/p will be  tuesday wednesday Thursday as value matches 2;
 now the default statment matches if nothing vale found ;for eg invalid input
 
