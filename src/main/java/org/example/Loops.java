45321c329152c3e34ab9f6efeb1e765ff6cfd466
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-01 15:54:23 +0530

    Added : LOOPS DO WHILE

--- src/main/java/org/example/Loops.java
+++ src/main/java/org/example/Loops.java
@@ -1,15 +1,23 @@
 package org.example;
 
+@SuppressWarnings("ALL")
 public class Loops {
     public static void main(String[] args) {
 //        int i = 0;
 //        for (int i = 100; i >=1; i=i-1) {
 //            System.out.println(i);
-//        }
-        int i =10;
-        while(i>1)
-        System.out.println(i);
-        i--;
+////        }
+//        int i =10;
+//        while(i>1)
+//        System.out.println(i);
+//        i--;
+//    }
+        int x = 10;
+        do {
+            System.out.println(x);
+            x = x - 1;
+            while(x >= 1) ;
+        }
     }
 }
 //prints 100 - 1
