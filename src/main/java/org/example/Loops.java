70825f1073b03326afdcd133681888b2df9dbe03
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-01 15:30:13 +0530

    Added : LOOPS 1 - 100 Type:3

--- src/main/java/org/example/Loops.java
+++ src/main/java/org/example/Loops.java
@@ -3,9 +3,11 @@ package org.example;
 public class Loops {
     public static void main(String[] args) {
 //        int i = 0;
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 100; i=i+1) {
             System.out.println(i);
         }
     }
 }
 //prints 1 -100
+//declaring type in for loop
+//i++ equals i= i+1
