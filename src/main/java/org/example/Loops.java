2c7e8a0256968219513b1e9af75d61ad03a62c59
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-05-01 15:26:59 +0530

    Added : LOOPS 1 - 100 Type:1

---
+++ src/main/java/org/example/Loops.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Loops {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//prints 1 -100
